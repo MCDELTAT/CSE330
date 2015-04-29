18e3a122a7c53c6aa9aa9a383458492e3ac1767f
Aaron C <aaronjchamberlain@gmail.com>
2015-04-29 08:58:28 -0700

    Removed time24 classes and updated to fix timeUnit bug

--- Lab01/lab1.cpp
+++ Lab01/lab1.cpp
@@ -6,21 +6,8 @@ int main(){
 
 	t.writeTime(); 		//Output: 8:15 AM
 	t.readTime();		//Input: 12:00 PM
-	t.addTime(180);		//add 3hours to t
-	t.writeTime();		//Output: 3:00 PM	
+	t.addTime(180);		//add 3 hours
+	t.writeTime();		//Output: 3:00 PM
+
 	return 0;
-
-	/* time12 t(11, 40, 0);
-	t.writeTime();
-	t.addTime(90);
-	t.writeTime();
-	std::cout << "\n";
-	t.readTime();
-	t.writeTime();
-	std::cout << "\n";
-	return 0; */
 }
-
-/* 
-Example: 
-*/
--- Lab01/time12.h
+++ Lab01/time12.h
@@ -2,11 +2,16 @@
 #include <iomanip>
 #include <string>
 
+
+//Note: Because I've neveer looked at my watch at midnight, I didn't notice:
+//Midnight is defined as 12:00AM the next day.... 12:00PM is noon.
+
 //maintains clock time
 class time12{
 	private:
 		int hour, minute;
 		bool timeUnit; //Used to store time period, 0=AM, 1=PM
+		bool backupTimeUnit;
 	public:
 		//Constructor
 		time12(int h=12, int m=0, bool tunit=0);
@@ -37,11 +42,23 @@ void time12::normalizeTime()
 	minute %= 60;
 
 	int addedTime= (hour + extraHours);
-   	if (addedTime >= 12 && timeUnit == 0){
+	//if the time added will make hours greater than 13, change the time Unit
+   	if (addedTime > 12 && timeUnit == 0){
+   		backupTimeUnit = 0; //backup for check later
    		timeUnit = 1;
    	}
-   	else if(addedTime >= 12 && timeUnit == 1){
+   	else if(addedTime > 12 && timeUnit == 1){
+   		backupTimeUnit = 1; //backup for check later
    		timeUnit = 0;
+   	}
+   	//to fix the changes time Unit incorrectly at 12 thing.
+   	//If it's AM and we add only one hour, make sure it's the same time unit
+   	if (hour==12 && extraHours<=11 && backupTimeUnit==0){
+   		timeUnit = 0;
+   	}
+   	//If it's PM and we add within the same timeUnit, ensure it stays the same
+   	else if (hour==12 && extraHours<=11 && backupTimeUnit==1){
+   		timeUnit = 1;
    	}
 
 	hour = addedTime % 12;
@@ -78,6 +95,9 @@ void time12::readTime(){
 //Output Time
 void time12::writeTime(){
 	std::cout << hour << ':';
+   	if (minute<=9){
+   		std::cout << "0"; //For single digit minute values, leading zero.
+   	}
    	//Output minute
    	std::cout << minute << "  ";
    	//Output Time Unit
