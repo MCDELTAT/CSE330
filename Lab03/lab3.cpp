fdf60f8cbca70d2895e37ddc8c4f3d81da5d4e21
U-CSEWIN\003392908 <003392908@jb360-27.cse.csusb.edu>
2015-05-13 13:22:37 -0700

    Switched to Richard's Version, final.

--- Lab03/lab3.cpp
+++ Lab03/lab3.cpp
@@ -1,86 +1,141 @@
-#include <iostream>
-#include <string>
-#include <stack>
+// Class: CSE 330
+// Term: Spring 2015
+// Instructor: George M. Georgiou
+// Name(s): Aaron Chamberlain & Richard Morones
+// Lab 3
+// Title:: Infix to postfix expression converstion
 
+#include<iostream>
+#include<stack>
+#include<string>
+ 
 using namespace std;
+ 
+// Function to convert Infix expression to postfix 
+string infix_postfix(string expression);
+ 
+// Function to verify whether an operator has higher precedence over other
+bool check_precedence(char operator1, char operator2);
+ 
+// Function to verify whether a character is operator symbol or not. 
+bool is_operator(char c);
+ 
+// Function to verify whether a character is alphanumeric chanaracter (letter or numeric digit) or not. 
+bool is_operand(char c);
+ 
+int main() 
+{
+	string expression; 
+	cout<<"Enter Expression \n";
+	getline(cin,expression);
+	string postfix = infix_postfix(expression);
+	cout<<"Output = "<<postfix<<"\n";
+}
+ 
+// Function to evaluate Postfix expression and return output
+string infix_postfix(string expression)
+{
+	// Declaring a Stack from Standard template library in C++.
+	stack<char> s;
+	string postfix = ""; // Initialize postfix as empty string.
+	for(int i = 0;i< expression.length();i++) {
+ 
+		// If character is operator, pop two elements from stack, perform operation and push the result back. 
+		if(is_operator(expression[i])) 
+		{
+			while(!s.empty() && s.top() != '(' && check_precedence(s.top(),expression[i]))
+			{
+				postfix+= s.top();
+				s.pop();
+			}
+			s.push(expression[i]);
+		}
+		// Else if character is an operand
+		else if(is_operand(expression[i]))
+		{
+			postfix +=expression[i];
+		}
+ 
+		else if (expression[i] == '(') 
+		{
+			s.push(expression[i]);
+		}
+ 
+		else if(expression[i] == ')') 
+		{
+			while(!s.empty() && s.top() !=  '(') {
+				postfix += s.top();
+				s.pop();
+			}
+			s.pop();
+		}
+		}
 
-int prec(char c){
-	int p;
-	if(c == '*' || c == '/'){
+	while(!s.empty()) {
+		postfix += s.top();
+		s.pop();
+ 
+	
+	}
+ 
+	return postfix;
+}
+	
+
+	
+// Function to verify whether a character is english letter or numeric digit. 
+// We are assuming in this solution that operand will be a single character
+bool is_operand(char C) 
+{
+	if(C == '+') return false;
+	if(C >= '0' && C <= '9') return true;
+	if(C >= 'a' && C <= 'z') return true;
+	if(C >= 'A' && C <= 'Z') return true;
+	return false;
+}
+ 
+// Function to verify whether a character is operator symbol or not. 
+bool is_operator(char C)
+{
+	if(C == '+' || C == '-' || C == '*' || C == '/')
+		return true;
+	else
+		return false;
+}
+ 
+// Function to get weight of an operator. An operator with higher weight will have higher precedence. 
+int precedence(char op)
+{
+	int p; 
+	switch(op)
+	{
+	case '+':
+	case '-':
+		p = 2;
+	case '*':
+	case '/':
 		p = 3;
-	}
-	else if(c == '+' || c == '-'){
-		p = 2;	
-	}
-	else if (c == '('){
+	case '(':
 		p = 1;
 	}
-	cout << p;
 	return p;
 }
-
-void infix_postfix(string s){
-	stack <char> ops;
-	int str_length = 0; //used to index loop
-	str_length = s.length(); //Careful: will not return expected value
-				 //with multi-bit characters included
-	int i = 0; //index
-
-	//start algorithm
-	while (i <= str_length){
-		//if not a operator => is operand
-		if (s[i] != '*' || '/' || '+' || '-' || '(' || ')'){
-			cout << s[i]; //print operand
-			i++; 
-		} 
-		else 
-			if (s[i] == '('){
-				ops.push(s[i]); //push operator onto stack
-				i++;
-			}
-			else if(s[i] == ')'){
-				while (!ops.empty() && ops.top() != '('){
-					cout << ops.top();
-					ops.pop();
-				}
-				if(!ops.empty()){
-					ops.pop();
-					i++;
-				}
-				else {
-					cout << "\n"; 
-					cout << "Error, no matching ( \n";
-					i++;
-				}
-			}
-			else if(ops.empty() || prec(ops.top())<prec(s[i])){
-				ops.push(s[i]);
-				i++;
-			}
-			else if(prec(ops.top()) >= prec(s[i])){
-				while (!ops.empty() && prec(ops.top()) >= 
-					prec(s[i]))
-				{
-					cout << ops.top();
-					ops.pop();
-				}
-				ops.push(s[i]);
-				i++;
-			}
-			//else check for errors
+ 
+// Function to perform an operation and return output. 
+bool check_precedence(char op1, char op2)
+{
+	int op1Weight = precedence(op1);
+	int op2Weight = precedence(op2);
+ 
+	// If operators have equal precedence, return true if they are left associative. 
+	// return false, if right associative. 
+	// if operator is left-associative, left one should be given priority. 
+	if(op1Weight == op2Weight)
+	{
+		return true;
 	}
-	while(!ops.empty()){
-		cout << ops.top();
-		ops.pop();
+	if (op1Weight > op2Weight){
+		return true;
 	}
-}
-
-int main(){
-	char a = '*';
-	prec(a);
-	cout << endl;
-	string b = "a+b-c";
-	infix_postfix(b);
-
-	return 0;
+	else return false;
 }
